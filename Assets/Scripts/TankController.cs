22980d8abf3533f5843c85d73ca4dd27ba65408e
Prajwal Ramappa <prajwal.ramappa@dumadu.com>
2022-04-16 09:46:26 +0530

    movement and rotation

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -6,12 +6,31 @@ public class TankController
     private TankModel tankModel;
     private TankView tankView;
 
+    private Rigidbody rb;
+
     public TankController(TankModel _tankModel,TankView _tankView)
     {
         tankModel = _tankModel;
-        tankView = _tankView;
+        tankView = GameObject.Instantiate<TankView>(_tankView);
+        rb = tankView.GetRigidbody();
         tankModel.SetTankController(this);
         tankView.SetTankController(this);
-        GameObject.Instantiate(tankView.gameObject);
+    }
+
+    public void Move(float movement,float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movementSpeed * movement;
+    }
+
+    public void Rotate(float rotation,float rotationSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotation * rotationSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+
+    public TankModel GetTankModel()
+    {
+        return tankModel;
     }
 }
